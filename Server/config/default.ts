4d467fe564fcadf937d3e5ec1e9d90026ec3fed6
mehdighachoui <mehdighachoui@gmail.com>
2023-05-17 18:38:57 +0100

    Creating  server  skeleton

---
+++ Server/config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    origin: process.env.ORIGIN,
+    port: process.env.PORT || 4000,
+};
---
+++ Server/src/app.ts
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+import express, { NextFunction, Request, Response } from 'express';
+import config from 'config';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import logger from './utils/logger';
+import validateEnv from './utils/validate-env';
+
+validateEnv()
+
+const app = express();
+const port = config.get<string>('port')
+
+
+async function InitMiddelwares() {
+
+    app.use(express.json({ limit: '10kb' }));
+
+    if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+    app.use(cookieParser());
+
+    app.use(
+        cors({
+            origin: config.get<string>('origin'),
+            credentials: true,
+        })
+    );
+}
+
+
+async function InitServer() {
+    try {
+        app.listen(port);
+        logger.info(`Server started  on http://localhost:${port}`);
+    }
+    catch (err) {
+        logger.info(`catch!  Server unable to start`, err);
+    }
+}
+
+async function main() {
+    InitMiddelwares()
+    InitServer()
+}
+
+main().catch((error) => {
+    logger.error(error instanceof Error ? error.message : error)
+    throw error;
+});
+
+
